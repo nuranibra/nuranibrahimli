997af07a8efe2fc11315b67ae896af3d61e678a7
Nuran28 <ibrahimovnuran008@gmail.com>
2024-05-20 23:21:03 +0400

    second commit

--- src/pages/techStack.js/techStack.js
+++ src/pages/techStack.js/techStack.js
@@ -7,6 +7,13 @@ import gitImage from './../../assets/image/skillsImage/logos_git-icon.png'
 import gitHubImage from './../../assets/image/skillsImage/akar-icons_github-fill.png'
 import bootStrapImage from './../../assets/image/skillsImage/logos_bootstrap.png'
 import vscodeImage from './../../assets/image/skillsImage/vscode-icons_file-type-vscode.png'
+import typeScriptImage from './../../assets/image/skillsImage/download.png'
+import dockerImage from './../../assets/image/skillsImage/docker.png'
+import expressImage from './../../assets/image/skillsImage/express.png'
+import nodejsImage from './../../assets/image/skillsImage/nodejs.png'
+import restApiImage from './../../assets/image/skillsImage/rest-api.png'
+import mysqlImage from './../../assets/image/skillsImage/mysql.png'
+import nextjsImage from './../../assets/image/skillsImage/nextjs.png'
 
 export default function TechStack () {
     return(
@@ -25,6 +32,13 @@ export default function TechStack () {
                     <img src={gitHubImage} alt='akar-icons_github-fill'/>
                     <img src={bootStrapImage} alt='logos_bootstrap'/>
                     <img src={vscodeImage} alt='vscode-icons_file-type-vscode'/>
+                    <img src={typeScriptImage} alt="TypeScript Image" />
+                    <img src={dockerImage} alt="Docker Image" />
+                    <img src={expressImage} style={{width:'300px'}} alt="Express.js Image" />
+                    <img src={nodejsImage} style={{width:'150px'}} alt="Node.js Image" />
+                    <img src={restApiImage} alt="REST API Image" />
+                    <img src={mysqlImage} alt="MySQL Image" />
+                    <img src={nextjsImage} alt="Next.js Image" />
                 </div>
             </div>
         </section>
